5b4de45bf00c84a41cdc0fa24e6da89495a0be8c
UnknownDeveloqer <aavault0@gmail.com>
2024-06-06 01:02:16 -0500

    Create RemoveExtraLanes.js

---
+++ RemoveExtraLanes.js
@@ -0,0 +1,6 @@
+// # INSTRUCTIONS
+// When in a game/round, inspect the page, then import the following code into the console and run the code.
+
+document.getElementsByClassName("pp-line pp-line1").remove()
+// If you want to remove the other extras:
+// document.getElementsByClassName("pp-key kkey").remove()
